f51e1e67e1368257ba5c052d47daaa04a1880a88
zayn-fastnu <f180114@cfd.nu.edu.pk>
2021-08-17 11:24:16 +0500

    Add cleanup

--- db/migrate/20210816121131_create_user_stocks.rb
+++ db/migrate/20210816121131_create_user_stocks.rb
@@ -1,8 +1,8 @@
 class CreateUserStocks < ActiveRecord::Migration[6.1]
   def change
     create_table :user_stocks do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :stock, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, :on_delete => :cascade
+      t.references :stock, null: false, foreign_key: true, :on_delete => :cascade
 
       t.timestamps
     end
