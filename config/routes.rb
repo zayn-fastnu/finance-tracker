93d1210d687c01812aa9fa5162958d1e17b5f279
zayn-fastnu <f180114@cfd.nu.edu.pk>
2021-08-17 15:38:15 +0500

    Add functionality to remove tracking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks, only: :create
+  resources :user_stocks, only: [:create, :destroy]
   #devise_for :users
   root "welcome#index"
   get '/welcome', to: "welcome#index"
@@ -14,10 +14,8 @@ Rails.application.routes.draw do
     get "my_portfolio" => "users/sessions"
     end
 end
-
 #do
  # devise_for :users
   #root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #end
-
